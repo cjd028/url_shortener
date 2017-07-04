e82c0d29ba12049cf79fc5a61ee8fafb5b7bd970
Chris <you@example.com>
2017-07-04 19:30:50 +0800

    created strong params for create method

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,4 +1,5 @@
 class UrlsController < ApplicationController
+
 	def index
         @urls = Url.all
     end
@@ -6,5 +7,24 @@ class UrlsController < ApplicationController
     def show
 
     	@urls = Url.find(params[:id])
+	end
+
+	def new
+  		@url = Url.new
+	end
+
+	def create
+  		@url = Url.new(params[:url_params])
+  		@url.shorten
+  	if @url.save
+      redirect_to @url  
+ 	else
+      render 'new'
+  	end   
+	end
+
+	private 
+	def url_params
+    	params.require(:url).permit(:long_url)
 	end
 end
